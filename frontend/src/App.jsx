bf0dac3a2954b510267d2d60a03e2591e27063f3
anscom <anscom.ooi@silverlakeaxis.com>
2025-03-06 16:29:32 +0800

    Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home.jsx';
 import Navbar from './components/Navbar/Navbar.jsx';
 import About from './pages/About/About.jsx';
+import Footer from './components/Footer/Footer.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
         <Route path='' element={<Home />}/>
         <Route path='/about' element={<About />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
